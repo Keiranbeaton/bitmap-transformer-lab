f9d8ffe6cd12d10ed48121953ed1f3d09692b4d5
keiran beaton <keiran beaton>
2016-07-18 10:51:18 -0700

    added a test

--- test/bitmap-test.js
+++ test/bitmap-test.js
@@ -1,2 +1,13 @@
-const expect = require('chai');
+const expect = require('chai').expect;
 const fs = require('fs');
+
+describe('transformed image created', function() {
+  var fileArray = [];
+  fs.readFile('../assets/transformed.bmp', function(err, data) {
+    fileArray.push(data);
+  });
+  it('should contain code to be sure that the transform has occurred', function(done) {
+    expect(fileArray[0].length).to.eql(30054);
+    done();
+  });
+});
